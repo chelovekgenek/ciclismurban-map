7b9ee98bfdbf072260c156656bdf4b063b3b5720
Evghenii Notarius <dufa456@gmail.com>
2019-07-29 00:19:55 +0200

    fix: content map markers listing keys

--- src/components/MainLayout/Content.tsx
+++ src/components/MainLayout/Content.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useMemo } from "react"
 
 import { Layout } from "components/generic/ui"
 import { GoogleMap } from "components/generic/GoogleMap"
@@ -12,28 +12,37 @@ import parkings from "mocks/parkings.json"
 import services from "mocks/services.json"
 import shops from "mocks/shops.json"
 
+const renderMarkers = <T extends {}>(type: string) => (point: T, i: number) => (
+  <GoogleMap.Marker key={`${type}__${i}`} position={point} />
+)
+
 interface IStateProps {
   filters: ReturnType<typeof getRoot>
 }
 
 interface IProps extends IStateProps {}
 
-export const Content: React.FC<IProps> = ({ filters }) => (
-  <Layout>
-    <Layout.Content>
-      <GoogleMap
-        containerElement={<Styled.MapContainer />}
-        mapElement={<Styled.Map />}
-        defaultZoom={14}
-        center={{ lat: 47.0203966, lng: 28.829422 }}
-      >
-        {filters.parkings && parkings.map(point => <GoogleMap.Marker position={point} />)}
-        {filters.services && services.map(point => <GoogleMap.Marker position={point} />)}
-        {filters.shops && shops.map(point => <GoogleMap.Marker position={point} />)}
-      </GoogleMap>
-    </Layout.Content>
-  </Layout>
-)
+export const Content: React.FC<IProps> = ({ filters }) => {
+  const parkingsMarkers = useMemo(() => filters.parkings && parkings.map(renderMarkers("parkings")), [filters.parkings])
+  const servicesMarkers = useMemo(() => filters.services && services.map(renderMarkers("services")), [filters.services])
+  const shopsMarkers = useMemo(() => filters.shops && shops.map(renderMarkers("shops")), [filters.shops])
+  return (
+    <Layout>
+      <Layout.Content>
+        <GoogleMap
+          containerElement={<Styled.MapContainer />}
+          mapElement={<Styled.Map />}
+          defaultZoom={14}
+          center={{ lat: 47.0203966, lng: 28.829422 }}
+        >
+          {parkingsMarkers}
+          {servicesMarkers}
+          {shopsMarkers}
+        </GoogleMap>
+      </Layout.Content>
+    </Layout>
+  )
+}
 
 export default connect<IStateProps, null, null, TAppState>(state => ({
   filters: getRoot(state),

c1930371b439948e66cc624c507c779f536cc912
Evghenii Notarius <dufa456@gmail.com>
2019-10-27 23:43:26 +0100

    feature: Sider select menu item

---
+++ src/components/generic/layout/App/Sider.spec.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+import { shallow } from "enzyme"
+
+import { Sider } from "./Sider"
+import * as Styles from "./Sider.styled"
+
+describe("pages/ServiceForm", () => {
+  const render = (props: Partial<React.ComponentProps<typeof Sider>> = {}) =>
+    shallow(<Sider match={{ params: {} } as any} history={{} as any} location={{} as any} {...props} />)
+  it("should match snapshot", () => {
+    expect(render()).toMatchSnapshot()
+  })
+  // it("should redirect to link when item was clicled", () => {
+  //   const props = { history: { push: jest.fn() } }
+  //   const wrapper = render(props as any)
+  //   const menuItem = wrapper.find(Styles.MenuItem).at(0)
+  //   menuItem.simulate("click")
+  //   expect(props.history.push).toBeCalledWith("/")
+  // })
+  it("should not set selected menu item if doesn't match with current route", () => {
+    const wrapper = render({ match: { path: "/asd", isExact: true, url: "/asd", params: {} } })
+    expect(wrapper.find(Styles.Menu).prop("selectedKeys")).toEqual([])
+  })
+  it("should not set selected menu item if it's matching with current route", () => {
+    const wrapper = render({ match: { path: "/", isExact: true, url: "/", params: {} } })
+    expect(wrapper.find(Styles.Menu).prop("selectedKeys")).toEqual(["/"])
+  })
+  it("should set selected menu item if current route is a children", () => {
+    const wrapper = render({
+      match: { path: "/event/:id", isExact: true, url: "/event/12345", params: { id: "12345" } },
+    })
+    expect(wrapper.find(Styles.Menu).prop("selectedKeys")).toEqual(["/events"])
+  })
+})
--- src/components/generic/layout/App/Sider.tsx
+++ src/components/generic/layout/App/Sider.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react"
+import React, { useState, useCallback, useMemo } from "react"
 import { withRouter, RouteComponentProps } from "react-router-dom"
 
 import { Icon } from "components/generic/ui"
@@ -9,12 +9,20 @@ import * as Styled from "./Sider.styled"
 
 interface IProps extends RouteComponentProps {}
 
-export const Sider: React.FC<IProps> = ({ history }) => {
+export const Sider: React.FC<IProps> = ({ history, match }) => {
   const [collapsed, setCollapsed] = useState(false)
   const handleClickItem = useCallback(({ key }: ClickParam) => history.push(key), [history])
+  const selected = useMemo(() => {
+    for (let option of options) {
+      if (option.link === match.path || (option.routes && option.routes.includes(match.path))) {
+        return [option.link]
+      }
+    }
+    return []
+  }, [match.path])
   return (
     <Styled.Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
-      <Styled.Menu>
+      <Styled.Menu selectedKeys={selected}>
         {options.map(item => (
           <Styled.MenuItem key={item.link} onClick={handleClickItem}>
             <Icon type={item.icon} />

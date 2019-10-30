aec2a35ca4ac614b773006efff8eeeacac599eed
Evghenii Notarius <dufa456@gmail.com>
2019-10-30 21:31:59 +0100

    fix(boot): redirect if no token and route matched

--- src/store/boot.ts
+++ src/store/boot.ts
@@ -1,6 +1,10 @@
 import { Store } from "redux"
+import { matchPath } from "react-router-dom"
 import { getFilters, CurrentActions } from "./entities/locations"
 import { getToken, LoginByTokenActions } from "./entities/user"
+import { history } from "./history"
+
+import routes, { indexRoutePath } from "components/App/Router.options"
 
 export const handleBoot = ({ dispatch, getState }: Store) => () =>
   new Promise(() => {
@@ -13,5 +17,10 @@ export const handleBoot = ({ dispatch, getState }: Store) => () =>
     }
     if (token) {
       dispatch(LoginByTokenActions.request())
+    } else {
+      const currentRoute = routes.find(item => matchPath(history.location.pathname, item))
+      if (currentRoute) {
+        history.replace(indexRoutePath)
+      }
     }
   })

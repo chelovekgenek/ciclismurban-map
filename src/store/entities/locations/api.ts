637509086e6eaee25d4e9d8fd49ab1656be30c49
chelovekgenek <dufa456@gmail.com>
2019-11-15 15:03:13 +0200

    fix: align introduced backend routes

--- src/store/entities/locations/api.ts
+++ src/store/entities/locations/api.ts
@@ -11,20 +11,21 @@ export const getParkingById = async (id: string) => request.get(`/parkings/${id}
 export const getServiceById = async (id: string) => request.get(`/services/${id}`)
 export const getShopById = async (id: string) => request.get(`/shops/${id}`)
 
-export const createEvent = async (payload: Partial<EventModel>) => request.post("/events", payload)
-export const updateEvent = async (id: string, payload: Partial<EventModel>) => request.patch(`/events/${id}`, payload)
-export const deleteEvent = async (id: string) => request.delete(`/events/${id}`)
+export const createEvent = async (payload: Partial<EventModel>) => request.post("/me/events", payload)
+export const updateEvent = async (id: string, payload: Partial<EventModel>) =>
+  request.patch(`/me/events/${id}`, payload)
+export const deleteEvent = async (id: string) => request.delete(`/me/events/${id}`)
 
-export const createParking = async (payload: Partial<ParkingModel>) => request.post("/parkings", payload)
+export const createParking = async (payload: Partial<ParkingModel>) => request.post("/me/parkings", payload)
 export const updateParking = async (id: string, payload: Partial<ParkingModel>) =>
-  request.patch(`/parkings/${id}`, payload)
-export const deleteParking = async (id: string) => request.delete(`/parkings/${id}`)
+  request.patch(`/me/parkings/${id}`, payload)
+export const deleteParking = async (id: string) => request.delete(`/me/parkings/${id}`)
 
-export const createService = async (payload: Partial<ServiceModel>) => request.post("/services", payload)
+export const createService = async (payload: Partial<ServiceModel>) => request.post("/me/services", payload)
 export const updateService = async (id: string, payload: Partial<ServiceModel>) =>
-  request.patch(`/services/${id}`, payload)
-export const deleteService = async (id: string) => request.delete(`/services/${id}`)
+  request.patch(`/me/services/${id}`, payload)
+export const deleteService = async (id: string) => request.delete(`/me/services/${id}`)
 
-export const createShop = async (payload: Partial<ShopModel>) => request.post("/shops", payload)
-export const updateShop = async (id: string, payload: Partial<ShopModel>) => request.patch(`/shops/${id}`, payload)
-export const deleteShop = async (id: string) => request.delete(`/shops/${id}`)
+export const createShop = async (payload: Partial<ShopModel>) => request.post("/me/shops", payload)
+export const updateShop = async (id: string, payload: Partial<ShopModel>) => request.patch(`/me/shops/${id}`, payload)
+export const deleteShop = async (id: string) => request.delete(`/me/shops/${id}`)
